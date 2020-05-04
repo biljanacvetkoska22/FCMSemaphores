13c1516980e92088eba28c8fca019e6745535826
Biljana Cvetkoska <biljana.cvetkoska21@gmail.com>
2020-05-04 16:07:45 +0200

    Initial commit

---
+++ MultithreadCopy/FileCopyMultithreaded.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FileCopyMultithreading
+{         
+    public class FileCopyMultithreaded
+    {
+        private delegate void CompleteDelegate(string sourceFile);
+        private static Queue<KeyValuePair<string, string>> CopyList = new Queue<KeyValuePair<string, string>>();
+        private static Semaphore pool = new Semaphore(0, 3);
+
+        public int BufferSizeInKB = 4 * 1024;
+        private int count;
+
+        private event CompleteDelegate FileCopyComplete;
+        private void OnFileCopyComplete(string sourceFile)
+        {
+            FileCopyComplete?.Invoke(sourceFile);
+        }
+
+        public void EnqueueFiles(KeyValuePair<string, string> file)
+        {
+            CopyList.Enqueue(file);
+        }
+
+        public FileCopyMultithreaded() { }
+
+       
+
+        private void CopyJob(object job)
+        {
+            KeyValuePair<string, string> copyJob = (KeyValuePair<string, string>)job;
+            CopyFile(copyJob);
+            count = CopyList.Count;
+        }
+
+        public void StartCopy()
+        {
+                      
+                        
+            while (CopyList.Count > 0)
+            {               
+                
+                
+                Thread t = new Thread(Copy);
+                t.Start(CopyList.Dequeue());
+                pool.WaitOne();
+            }
+
+            
+            while (count > 0)
+            {
+                Thread.Sleep(100);
+            }
+        }
+
+        private void Copy(object x)
+        {            
+            if (x is KeyValuePair<string, string>)
+            {
+                var kvp = (KeyValuePair<string, string>) x;
+                CopyFile(kvp);
+                pool.Release();
+                Interlocked.Decrement(ref count);
+            }
+        }
+
+        private void CopyFile(KeyValuePair<string, string> copyJob)
+        {
+            byte[] buffer = new byte[BufferSizeInKB * 1024]; // 4K buffer
+
+            using (FileStream source = new FileStream(copyJob.Key, FileMode.Open, FileAccess.Read))
+            {
+                using (FileStream destination = new FileStream(copyJob.Value, FileMode.Create, FileAccess.Write))
+                {
+                    int currentBlockSize = 0;
+
+                    while ((currentBlockSize = source.Read(buffer, 0, buffer.Length)) > 0)
+                    {
+                        destination.Write(buffer, 0, currentBlockSize);
+                    }
+                }
+            }
+
+            OnFileCopyComplete(copyJob.Key);
+        }
+    }
+}
---
+++ MultithreadCopy/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using FileCopyMultithreading;
+
+namespace MultithreadCopy
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            FileCopyMultithreaded fcm = new FileCopyMultithreaded();
+
+            string sourceDirectory = @"D:\sourceDirectory";
+            string targetDirectory = @"D:\targetDirectory";
+
+            if (!Directory.Exists(sourceDirectory))
+            {
+                Console.WriteLine("The specified source directory does not exist!");
+                Environment.Exit(-1);
+            }
+            if (!Directory.Exists(targetDirectory))
+            {
+                Directory.CreateDirectory(targetDirectory);
+            }
+
+            foreach (string sourceFile in Directory.GetFiles(sourceDirectory))
+            {
+                string targetFile = Path.Combine(targetDirectory, Path.GetFileName(sourceFile));
+                fcm.EnqueueFiles(new KeyValuePair<string, string>(sourceFile, targetFile));
+            }
+
+            
+            if (fcm != null) 
+                fcm.StartCopy();
+
+
+            Console.WriteLine( "Press enter to end");
+            Console.ReadKey(); // We need to wait for threads to finish their copying.
+        }
+    }
+}
